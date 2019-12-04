4f804151a3c36f6aea2329c57f55d4919694ba43
vedika72 <36006164+vedika72@users.noreply.github.com>
2019-12-04 17:14:36 +0530

    filtered reviews on the basis of max similar categories restaurants

--- yelp1.py
+++ yelp1.py
@@ -9,6 +9,8 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
+import operator
+
 # open input file: 
 ifile = open('yelp_dataset/business.json', encoding = 'utf-8') 
 # read the first 70k entries
@@ -19,6 +21,9 @@ stateList = []
 businesses = []
 categoryList = []
 favBusiness = []
+
+our_business_id = 'SBNucLXc9dQP6VBj__XOmQ'
+our_business_categories = []
 
 #to mine relevant business IDs by filtering through categories
 fCategories = "Restaurants, Pizza, Cafes, Bars & Pubs, Bars, Burgers, Diners, Breakfast & Brunch, Chinese, Chicken Wings, Italian,  Desserts, Caterers, Bakeries, Waffles, American (Traditional), Soup, Comfort food, Sandwiches, Steak houses, Glutten free, French, Fast food, Salad, Bagels, Coffee and tea, Vegan, Lounges, Speciality food, Sea food, Food trucks, Mexican, Tapas, Asian Fusion"
@@ -54,11 +59,13 @@ for i, line in enumerate(ifile):
         if state== 'AZ':
             favBusiness.append(ID)
             categoryList.append(categ)
-  
+            
+    if ID== our_business_id:
+        our_business_categories = categ
+        
     stateList.append(state)
     businesses.append(ID)
     # add to the data collected so far
-
     
     all_data.append([ID, name, state])
     
@@ -67,9 +74,38 @@ fav_data = []
 # create the DataFrame
 df = pd.DataFrame(all_data, columns=['ID' ,'name','state'])
 print(df)
-
-# df.to_hdf('revie20ws.h5','reviews')
 ifile.close()
+# df.to_hdf('revie20ws.h5','reviews')
+
+# get the count of categories overlapping with our business
+size = len(favBusiness)
+similarity_count = []
+similar_business_id = []
+for i in range(size):
+    id_category = []
+    id_category = categoryList[i]
+    count = 0
+    for j in id_category:
+        for k in our_business_categories:
+            if k==j:
+                count += 1
+    similarity_count.append(count)
+    similar_business_id.append(favBusiness[i])
+
+# dictionary with business and their corresponding similar categories count
+    
+similarity_dict = dict(zip(similar_business_id, similarity_count))
+    
+sorted_similarity_dict = sorted(similarity_dict.items(), key=operator.itemgetter(1))
+
+# collecting business ids with max similarity
+threshold = 50
+max_similar_businesses = []
+max_counts = []
+for x in list(reversed(list(sorted_similarity_dict)))[1:threshold+1]:
+    max_similar_businesses.append(x[0])
+
+
 all_data = []
 ifile = open('yelp_dataset/review.json', encoding = 'utf-8') 
 stop = 100000
@@ -88,11 +124,12 @@ for i, line in enumerate(ifile):
     ID = data['business_id']
     text = data['text']
     
-    for f_id in favBusiness:    
-        if ID == f_id:
-            reviews.append(text)
-            businessIdOfReview.append(ID)
-            all_data.append([ID, text])
+    if ID in max_similar_businesses:
+        print(ID)
+        reviews.append(text)
+        businessIdOfReview.append(ID)
+        all_data.append([ID, text])
+ 
     
 # create the DataFrame
 df = pd.DataFrame(all_data, columns=['ID','text'])
@@ -103,4 +140,4 @@ Counter(res.values())
 
 
 # df.to_hdf('revie20ws.h5','reviews')
-ifile.close() 
+ifile.close()  
